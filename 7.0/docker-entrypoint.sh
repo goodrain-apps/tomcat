9ab906553728a608ceb35f3f3e439af9694b6623
zhouyq <zhouyq@goodrain.com>
2017-12-22 16:33:04 +0800

    [ADD] add redis seesion

--- 7.0/docker-entrypoint.sh
+++ 7.0/docker-entrypoint.sh
@@ -7,4 +7,21 @@ if [ "$ENABLE_APM" == "true" ];then
     sed -i "2 a. /usr/local/tomcat/bin/pinpoint-agent.sh" /usr/local/tomcat/bin/catalina.sh 
 fi
 
+# redis sersion
+if [ "$REDIS_SESSION" == "true" ];then
+sed -i 'd#</Context>#' /usr/local/tomcat/conf/context.xml
+
+cat >>  /usr/local/tomcat/conf/context.xml<< END
+<Valve className="com.orangefunction.tomcat.redissessions.RedisSessionHandlerValve" />
+<Manager className="com.orangefunction.tomcat.redissessions.RedisSessionManager"
+             host="127.0.0.1" 
+             port="6379" 
+             database="0" 
+             maxInactiveInterval="60"  />
+</Context>
+
+END
+
+fi
+
 exec $@
