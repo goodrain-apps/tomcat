3429dd30c8b5bc53800a63fa0016d98989ff9d74
zhouyq <zhouyq@goodrain.com>
2017-12-23 10:42:03 +0800

    [ADD] 8.5 添加pinpoint支持

---
+++ 8.5/docker-entrypoint.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+[[ $DEBUG ]] &&  set -x
+
+# detect ENABLE_APM env
+if [ "$ENABLE_APM" == "true" ];then
+    sed -i "2 a. /usr/local/tomcat/bin/pinpoint-agent.sh" /usr/local/tomcat/bin/catalina.sh 
+fi
+
+# redis sersion
+if [ "$REDIS_SESSION" == "true" ];then
+sed -i 's#</Context>##' /usr/local/tomcat/conf/context.xml
+
+cat >>  /usr/local/tomcat/conf/context.xml << END
+<Valve className="com.orangefunction.tomcat.redissessions.RedisSessionHandlerValve" />
+<Manager className="com.orangefunction.tomcat.redissessions.RedisSessionManager"
+             host="127.0.0.1" 
+             port="6379" 
+             database="0" 
+             maxInactiveInterval="60"  />
+</Context>
+
+END
+
+fi
+
+exec $@
