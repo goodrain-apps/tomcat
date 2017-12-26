d86ac39f664df49abd3ab3ceb1c8ad0e79e19dd9
zhouyq <zhouyq@goodrain.com>
2017-12-26 17:44:32 +0800

    [FIX] 修改pinpoint-agent参数

--- 7.0/docker-entrypoint.sh
+++ 7.0/docker-entrypoint.sh
@@ -6,13 +6,14 @@
 if [ "$ENABLE_APM" == "true" ];then
     COLLECTOR_IP=${COLLECTOR_IP:-127.0.0.1}
     sed -i "2 a. /usr/local/tomcat/bin/pinpoint-agent.sh" /usr/local/tomcat/bin/catalina.sh
-    sed -i -r "s/(profiler.collector.ip)=.*/\1=${COLLECTOR_IP}/" /usr/local/pinpoint-agent/pinpoint.config
+    sed -i -r -e "s/(profiler.collector.ip)=.*/\1=${COLLECTOR_IP}/" \
+              -e "s/#(profiler.applicationservertype=TOMCAT)/\1/" /usr/local/pinpoint-agent/pinpoint.config
 fi
 
 
 # redis sersion
 if [ "$REDIS_SESSION" == "true" ];then
-sed -i 's#</Context>##' /usr/local/tomcat/conf/context.xml
+    sed -i 's#</Context>##' /usr/local/tomcat/conf/context.xml
 
 cat >>  /usr/local/tomcat/conf/context.xml << END
 <Valve className="com.orangefunction.tomcat.redissessions.RedisSessionHandlerValve" />
