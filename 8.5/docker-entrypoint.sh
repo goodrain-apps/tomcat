62428280192db53430349fec254cc1cc938c3158
guox <guox@goodrain.com>
2019-06-11 00:22:20 +0800

    change sth about pinpoint-agent

--- 8.5/docker-entrypoint.sh
+++ 8.5/docker-entrypoint.sh
@@ -9,9 +9,19 @@ REDIS_PORT=${REDIS_URL#*:}
 
 # detect ENABLE_APM env
 if [ "$ENABLE_APM" == "true" ];then
-    COLLECTOR_IP=${COLLECTOR_IP:-127.0.0.1}
+    COLLECTOR_TCP_HOST=${COLLECTOR_TCP_HOST:-127.0.0.1}
+    COLLECTOR_TCP_PORT=${COLLECTOR_TCP_PORT:-9994}
+    COLLECTOR_UDP_SPAN_LISTEN_HOST=${COLLECTOR_UDP_SPAN_LISTEN_HOST:-127.0.0.1}
+    COLLECTOR_UDP_SPAN_LISTEN_PORT=${COLLECTOR_UDP_SPAN_LISTEN_PORT:-9996}
+    COLLECTOR_UDP_STAT_LISTEN_HOST=${COLLECTOR_UDP_STAT_LISTEN_HOST:-127.0.0.1}
+    COLLECTOR_UDP_STAT_LISTEN_PORT=${COLLECTOR_UDP_STAT_LISTEN_PORT:-9995}
     sed -i "2 a. /usr/local/tomcat/bin/pinpoint-agent.sh" /usr/local/tomcat/bin/catalina.sh
-    sed -i -r "s/(profiler.collector.ip)=.*/\1=${COLLECTOR_IP}/" /usr/local/pinpoint-agent/pinpoint.config
+    sed -i -r -e "s/(profiler.collector.ip)=.*/\1=${COLLECTOR_TCP_HOST}/" \
+              -e "s/(profiler.collector.tcp.port)=.*/\1=${COLLECTOR_TCP_PORT}/" \
+              -e "s/(profiler.collector.span.port)=.*/\1=${COLLECTOR_UDP_SPAN_LISTEN_PORT}/" \
+              -e "s/(profiler.collector.stat.port)=.*/\1=${COLLECTOR_UDP_STAT_LISTEN_PORT}/" /usr/local/pinpoint-agent/pinpoint.config
+    export APP_NAME=${APP_NAME:-${SERVICE_NAME:-${HOSTNAME}}}
+    export AGENT_ID=${APP_NAME}-${HOST_IP}
 fi
 
 # redis sersion
